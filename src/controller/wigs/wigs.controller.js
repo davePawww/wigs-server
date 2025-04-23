4d76f4228d13af02bc4eea321aef8361f8cde1c4
DavePaurillo <paurillo.dave@gmail.com>
2025-04-24 02:47:35 +0800

    chore: create, update, delete backend functionality

--- src/controller/wigs/wigs.controller.js
+++ src/controller/wigs/wigs.controller.js
@@ -17,3 +17,95 @@ export const getUserWigs = async (req, res) => {
 		})
 	}
 }
+
+export const createWig = async (req, res) => {
+	const id = req.auth.userId
+	const { description } = req.body
+
+	if (!description) {
+		return res.status(400).json({
+			success: false,
+			message: "Description is required",
+		})
+	}
+
+	try {
+		const wig = await Wigs.create({
+			description,
+			user: id,
+		})
+
+		res.status(201).json({
+			success: true,
+			message: "Wig created successfully",
+			wig: wig,
+		})
+	} catch (error) {
+		console.error("Failed to create wig: ", error)
+		res.status(500).json({
+			success: false,
+			message: "Failed to create wig",
+		})
+	}
+}
+
+export const updateWig = async (req, res) => {
+	const { _id, description, completed } = req.body
+
+	try {
+		const wig = await Wigs.findById(_id)
+		if (!wig) {
+			return res.status(404).json({
+				success: false,
+				message: "Wig not found",
+			})
+		}
+
+		if (!description) {
+			return res.status(400).json({
+				success: false,
+				message: "Description is required",
+			})
+		}
+
+		wig.description = description
+		wig.completed = completed
+		await wig.save()
+
+		res.status(200).json({
+			success: true,
+			message: "Wig updated successfully",
+			wig: wig,
+		})
+	} catch (error) {
+		console.error("Failed to update wig: ", error)
+		res.status(500).json({
+			success: false,
+			message: "Failed to update wig",
+		})
+	}
+}
+
+export const deleteWig = async (req, res) => {
+	const { id } = req.params
+	try {
+		const wig = await Wigs.findByIdAndDelete(id)
+		if (!wig) {
+			return res.status(404).json({
+				success: false,
+				message: "Wig not found",
+			})
+		}
+
+		res.status(200).json({
+			success: true,
+			message: "Wig deleted successfully",
+		})
+	} catch (error) {
+		console.error("Failed to delete wig: ", error)
+		res.status(500).json({
+			success: false,
+			message: "Failed to delete wig",
+		})
+	}
+}
--- src/controller/wigs/wigs.route.js
+++ src/controller/wigs/wigs.route.js
@@ -1,9 +1,17 @@
 import { Router } from "express"
-import { getUserWigs } from "./wigs.controller.js"
+import {
+	getUserWigs,
+	createWig,
+	updateWig,
+	deleteWig,
+} from "./wigs.controller.js"
 import { authorizeJWT } from "../../middleware/auth.js"
 
 const router = Router()
 
 router.get("/getUserWigs", authorizeJWT, getUserWigs)
+router.post("/createWig", authorizeJWT, createWig)
+router.put("/updateWig", authorizeJWT, updateWig)
+router.delete("/deleteWig/:id", authorizeJWT, deleteWig)
 
 export default router
